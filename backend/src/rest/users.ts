04e4fda9af5b5ed51bcad2277a65cdcc399a7d65
Campbell Timm <campbell.timm@gmail.com>
2023-04-28 17:11:53 +1000

    a

--- backend/src/rest/users.ts
+++ backend/src/rest/users.ts
@@ -8,7 +8,7 @@ export let cookieOptions = {
   maxAge: 1000 * 60 * 60,
   httpOnly: true, 
   signed: true,
-  sameSite: false
+  sameSite: 'none' as 'none'
 }
 
 const getFavourites = async (request: Request, response: Response) => {
