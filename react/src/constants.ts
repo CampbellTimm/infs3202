a2c3b97d03b1efb1cd5a5b0b966a148c303230a7
Campbell Timm <campbell.timm@gmail.com>
2023-04-28 17:28:18 +1000

    a

--- react/src/constants.ts
+++ react/src/constants.ts
@@ -1,2 +1,2 @@
 // export const API_URL = "http://127.0.0.1:4000"
-export const API_URL = "http://ec2-3-25-107-76.ap-southeast-2.compute.amazonaws.com"
+export const API_URL = "https://ec2-3-25-107-76.ap-southeast-2.compute.amazonaws.com"
